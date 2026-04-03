26d968c752e27e75daf9927c324d98e6c33b7398
Guillermina Gatti <yimina777@gmail.com>
2026-04-03 11:45:55 -0300

    Entrega 4

---
+++ src/CuartaEntrega.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CuartaEntrega {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //        System.out.println("Escribí tu nombre:");
+        //        String nombre = sc.nextLine();
+
+        //Clase 7
+//        1. Crea un array de cuatro películas e imprimirlas por orden alfabético utilizando for-each.
+
+//        2. Crea un nuevo array que contenga la lista de películas en mayúscula.
+
+//        3. Crea una copia para incluir dos títulos más, visualizarlo con un metodo de Arrays.
+
+//        4. Crea una variable que guardará la primera película que comience con “L”, se debe recorrer el array
+//        buscando el primer resultado.
+
+//        5. Pedir 3 notas(Scanner), guardarlas en un array y calcular el promedio. A continuación mostrar el
+//        mayor y el menor.
+
+        //Clase 8
+//        Utilizando la clase Scanner:
+//        1. Escribir una función para calcular la cantidad de faltas disponibles de acuerdo a
+//        un porcentaje de asistencia y cantidad de encuentros determinado¿Cuál es el
+//        resultado para una cursada de 23 encuentros que requiere un 75% de asistencia? ¿Y
+//        para el caso de este módulo?
+
+//        2. Ahora requerimos una función en la cual se solicite al usuario que ingrese
+//        hasta cinco destinos turísticos por separado validando lo ingresado. Debe
+//        guardarlos en un nuevo Array retornando la información del mismo como un texto.
+
+    }
+}
